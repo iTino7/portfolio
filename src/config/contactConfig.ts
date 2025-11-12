c8ec58cb5c6e523a2a55f1da3459682803eb6d1f
iTino7 <Sabatino.b007@outlook.it>
2025-11-12 11:57:09 +0100

    update contactConfig

--- src/config/contactConfig.ts
+++ src/config/contactConfig.ts
@@ -2,5 +2,5 @@ export const CONTACT_API_URL =
   import.meta.env.VITE_CONTACT_API_URL?.toString() ??
   (typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:3002/api/contact"
-    : `${typeof window !== "undefined" ? window.location.origin.replace(/\/$/, "") : ""}/api/contact`)
+    : "https://portfolio-be-h15w.onrender.com/api/contact")
 
